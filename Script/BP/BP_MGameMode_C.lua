37e1ddc04debce736788a281200126ea84f501d0
lupin3 <moodyliu@yeah.net>
2021-10-31 19:57:36 +0800

    init

---
+++ Script/BP/BP_MGameMode_C.lua
@@ -0,0 +1,48 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+require "UnLua"
+-- if WITH_LUA_DEBUG then
+--     require("LuaPanda").start("127.0.0.1",8818)
+-- end
+
+local BP_MGameMode_C = Class()
+
+--function BP_MGameMode_C:Initialize(Initializer)
+--end
+
+--function BP_MGameMode_C:UserConstructionScript()
+--end
+
+function BP_MGameMode_C:ReceiveBeginPlay()
+	self.Overridden.ReceiveBeginPlay(self)
+    if not BUILD_SHIPPING then
+        require "LuaPanda"
+        -- LuaPanda.start("127.0.0.1",8818)
+    end
+    print('hello world!')
+end
+
+--function BP_MGameMode_C:ReceiveEndPlay()
+--	self.Overridden.ReceiveEndPlay(self)	
+--end
+
+-- function BP_MGameMode_C:ReceiveTick(DeltaSeconds)
+--	self.Overridden.ReceiveTick(self,DeltaSeconds)	
+-- end
+
+--function BP_MGameMode_C:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
+--end
+
+--function BP_MGameMode_C:ReceiveActorBeginOverlap(OtherActor)
+--end
+
+--function BP_MGameMode_C:ReceiveActorEndOverlap(OtherActor)
+--end
+
+return BP_MGameMode_C
